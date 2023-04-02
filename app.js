ee5e961d5e35e3f7c183884dbdc8a513a44beded
ErhNay <erhnay86@gmail.com>
2023-04-02 19:03:39 +0000

    Make the work section responsive

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let burger = document.querySelector('.hamburger');
 let burgerFirstLine = document.querySelector('.hamburger__line:nth-child(1)');
 let burgerSecondLine = document.querySelector('.hamburger__line:nth-child(2)');
 let burgerThirdLine = document.querySelector('.hamburger__line:nth-child(3)');
+let links = document.querySelectorAll('.header__nav--link');
 
 burger.addEventListener('click', function () {
   burgerFirstLine.classList.toggle('hamburger__line-1');
